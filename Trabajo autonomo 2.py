2e9c8d79ffe6238fffb44c0cc0df33489431e660
SaulAlvarez32 <151982333+SaulAlvarez32@users.noreply.github.com>
2023-11-24 23:36:11 -0500

    Add files via upload

---
+++ Trabajo autonomo 2.py
@@ -0,0 +1,11 @@
+print("¡Bienvenido al juego de Piedra, Papel o Tijeras!")
+print("Reglas:")
+print("  - Piedra aplasta Tijeras.")
+print("  - Tijeras cortan Papel.")
+print("  - Papel envuelve Piedra.")
+a="Empezar la partida o"
+b="(Salir de la aplicacion)"  
+print(a,b)
+c="Ingresar nombre de usuario"
+d="(Digite su usuario)"
+print(c,d)
